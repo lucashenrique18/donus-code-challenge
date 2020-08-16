cc79309cd8dbc9f5c7af034949aaae5be0960643
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 02:16:41 -0300

    feat: ensure SignUpController returns 500 if CpfValidator throws

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { CpfValidator } from '../../protocols/cpf-validator'
 import { InvalidParamError } from '../../errors/invalid-param-error'
@@ -13,15 +13,20 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'cpf', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try{
+      const requiredFields = ['name', 'cpf', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const isValidCpf = this.cpfValidator.isValid(httpRequest.body.cpf)
-    if (!isValidCpf) {
-      return badRequest(new InvalidParamError('cpf'))
+      const isValidCpf = this.cpfValidator.isValid(httpRequest.body.cpf)
+      if (!isValidCpf) {
+        return badRequest(new InvalidParamError('cpf'))
+      }
+    } catch (error) {
+      console.error(error)
+      return serverError()
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,14 @@
 import { HttpResponse } from '../protocols/http'
+import { ServerError } from '../errors/server-error'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
 })
 
 export const ok = (data: any): HttpResponse => ({
