a919366f6329629c79cee022dc97d8add1cc8b0e
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 18:50:16 -0300

    test: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -95,4 +95,20 @@ describe('DbAddAccount Usecase', () => {
     await expect(promise).rejects.toThrow()
   })
 
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      cpf: 'valid_cpf',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      cpf: 'valid_cpf',
+      password: 'hashed_password'
+    })
+  })
+
 })
