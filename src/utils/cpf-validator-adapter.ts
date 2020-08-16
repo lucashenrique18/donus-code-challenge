fc67189fcc35461950cdec08f4d325e8593a2871
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 04:34:25 -0300

    feat: ensure CpfValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator-adapter.ts
+++ src/utils/cpf-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { CpfValidator } from '../presentation/protocols/cpf-validator'
+import * as validator from '@fnando/cpf'
 
 export class CpfValidatorAdapter implements CpfValidator {
   isValid (cpf: string): boolean {
-    return false
+    return validator.isValid(cpf)
   }
 }
