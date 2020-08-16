90512cb0433b1b3e44bb045e26046176e8601a58
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 01:59:22 -0300

    feat: ensure SignUpController returns 400 if an invalid cpf is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -2,14 +2,26 @@ import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
+import { CpfValidator } from '../../protocols/cpf-validator'
+import { InvalidParamError } from '../../errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+  private readonly cpfValidator: CpfValidator
+
+  constructor (cpfValidator: CpfValidator) {
+    this.cpfValidator = cpfValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'cpf', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isValidCpf = this.cpfValidator.isValid(httpRequest.body.cpf)
+    if (!isValidCpf) {
+      return badRequest(new InvalidParamError('cpf'))
     }
   }
 }
