e2a56dacd958cb171974dc6ebacb9f3f3161286b
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 04:34:16 -0300

    feat: ensure CpfValidatorAdapter returns true if validator returns true

--- src/utils/cpf-validator.spec.ts
+++ src/utils/cpf-validator.spec.ts
@@ -1,4 +1,11 @@
 import { CpfValidatorAdapter } from './cpf-validator-adapter'
+import validator from '@fnando/cpf'
+
+jest.mock('@fnando/cpf', () => ({
+  isValid (): boolean {
+    return true
+  }
+}))
 
 const makeSut = (): CpfValidatorAdapter => {
   return new CpfValidatorAdapter()
@@ -7,8 +14,22 @@ const makeSut = (): CpfValidatorAdapter => {
 describe('CpfValidator Adapter', () => {
   test('Should return false if validator returns false ', () => {
     const sut = makeSut()
-    const isEmailValid = sut.isValid('invalid_cpf')
-    expect(isEmailValid).toBe(false)
+    jest.spyOn(validator, 'isValid').mockReturnValueOnce(false)
+    const isCpfValid = sut.isValid('invalid_cpf')
+    expect(isCpfValid).toBe(false)
+  })
+
+  test('Should return false if validator returns true ', () => {
+    const sut = makeSut()
+    const isCpfValid = sut.isValid('valid_cpf')
+    expect(isCpfValid).toBe(true)
+  })
+
+  test('Should call validator with correct cpf', () => {
+    const sut = makeSut()
+    const isCpfValidSpy = jest.spyOn(validator, 'isValid')
+    sut.isValid('any_cpf')
+    expect(isCpfValidSpy).toHaveBeenCalledWith('any_cpf')
   })
 
 })
