445ea260d28b77d3a8e7c20778f02bed6a72b4bf
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 00:48:20 -0300

    refactor: move SignUpController to your own folder

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,6 +1,6 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
