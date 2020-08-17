573d248b9483f8a2b15350c25cfcef889dcfe33b
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-16 23:48:38 -0300

    refactor: remove unused vars

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
 import { MongoClient, Collection } from 'mongodb'
-import { AccountModel } from '../../../../domain/models/account-model'
 
 export const MongoHelper = {
   client: null as MongoClient,
