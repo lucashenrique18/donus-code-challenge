6365d44f2e96e3a2e44b6b4f0aa0295ad8bc8362
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-17 23:39:43 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { CpfValidatorAdapter } from '../../utils/cpf-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const cpfValidatorAdapter = new CpfValidatorAdapter()
+  const bcryptAdapter = new BcryptAdapter(12)
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  return new SignUpController(cpfValidatorAdapter, dbAddAccount)
+}
