664dc19863192136c7cdb4742c9cd07ad7ad649e
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-19 14:21:37 -0300

    test: ensure DepositController returns 400 if no password is provided

--- src/presentation/controllers/deposit/deposit.spec.ts
+++ src/presentation/controllers/deposit/deposit.spec.ts
@@ -14,4 +14,18 @@ describe('Deposit Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('cpf'))
   })
+
+  test('Should return 400 if no password is provided', async () => {
+    const sut = new DepositController()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        depositValue: 100.00
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
 })
