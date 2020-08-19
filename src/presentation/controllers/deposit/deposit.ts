f2010a1a9bcd35b18f4725bbafa70387b6a4b568
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-19 14:21:04 -0300

    feat: ensure DepositController returns 400 if no password is provided

--- src/presentation/controllers/deposit/deposit.ts
+++ src/presentation/controllers/deposit/deposit.ts
@@ -5,7 +5,7 @@ import { badRequest } from '../../helpers/http-helper'
 export class DepositController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['cpf']
+    const requiredFields = ['cpf', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
