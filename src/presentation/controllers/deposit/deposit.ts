9606aedb252cd99dad0f00a5944f5fe09a3ab0d3
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-19 19:04:39 -0300

    feat: ensure DepositController calls CpfValidator with correct cpf

--- src/presentation/controllers/deposit/deposit.ts
+++ src/presentation/controllers/deposit/deposit.ts
@@ -1,8 +1,14 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { MissingParamError } from '../../errors'
+import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { CpfValidator } from '../../protocols/cpf-validator'
 
 export class DepositController implements Controller {
+  private readonly cpfValidator: CpfValidator
+
+  constructor (cpfValidator: CpfValidator) {
+    this.cpfValidator = cpfValidator
+  }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['cpf', 'password', 'depositValue']
@@ -10,6 +16,11 @@ export class DepositController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const { cpf } = httpRequest.body
+    const isValidCpf = this.cpfValidator.isValid(cpf)
+    if (!isValidCpf) {
+      return badRequest(new InvalidParamError('cpf'))
     }
     return null
   }
