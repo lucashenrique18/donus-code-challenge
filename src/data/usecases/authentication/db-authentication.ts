ed48ee671abfdb977f61d865229d219fc935835b
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-20 20:14:01 -0300

    feat: ensure DbAuthentication calls HashComparer with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,15 +1,21 @@
 import { Authentication } from '../../../domain/usecases/authentication/authentication'
 import { LoadAccountByCpfRepository } from '../../protocols/load-account-by-cpf-repository';
+import { HashComparer } from '../../protocols/hash-comparer';
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository
-  constructor (loadAccountByCpfRepository: LoadAccountByCpfRepository) {
+  private readonly hashComparer: HashComparer
+  constructor (loadAccountByCpfRepository: LoadAccountByCpfRepository, hashComparer: HashComparer) {
     this.loadAccountByCpfRepository = loadAccountByCpfRepository
+    this.hashComparer = hashComparer
   }
 
   async auth (cpf: string, password: string): Promise<boolean> {
-    await this.loadAccountByCpfRepository.load(cpf)
-    return null
+    const account = await this.loadAccountByCpfRepository.load(cpf)
+    if (!account) {
+      return null
+    }
+    await this.hashComparer.compare(password, account.password)
   }
 
 }
