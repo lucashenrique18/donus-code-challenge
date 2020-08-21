cef1971afd8023418dcd73b1c21424b53a65eed1
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-21 17:53:36 -0300

    test: ensure deposit route return 200 on success

---
+++ src/main/routes/deposit-routes.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
+
+describe('Deposit Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return 200 on success', async () => {
+    const password = await hash('123', 12)
+    await accountCollection.insertOne({
+      name: 'Lucas',
+      cpf: '032.561.179-37',
+      password
+    })
+    await request(app)
+      .post('/api/deposit')
+      .send({
+        cpf: '032.561.179-37',
+        password: '123',
+        depositValue: 100
+      })
+      .expect(200)
+  })
+
+})
