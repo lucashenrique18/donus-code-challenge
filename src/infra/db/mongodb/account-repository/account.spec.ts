a808ca9e036d6aa2a94608a3845e671b7998e654
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-21 12:26:03 -0300

    test: ensure AccountMongoRepository returns an account on loadByCpf success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,5 +1,8 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
+import { Collection } from 'mongodb'
+
+let accountCollection: Collection
 
 describe('Account Mongo Repository', () => {
 
@@ -12,17 +15,32 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on success', async () => {
+  test('Should return an account on add success', async () => {
     const sut = new AccountMongoRepository()
     const account = await sut.add({
       name: 'any_name',
       cpf: 'any_cpf',
       password: 'any_password'
     })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.cpf).toBe('any_cpf')
+    expect(account.password).toBe('any_password')
+  })
+
+  test('Should return an account on loadByCpf success', async () => {
+    const sut = new AccountMongoRepository()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      cpf: 'any_cpf',
+      password: 'any_password'
+    })
+    const account = await sut.loadByCpf('any_cpf')
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
