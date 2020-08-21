3a6c6280b2a0c759f01260d4060c33ae023e51f4
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-21 14:04:25 -0300

    feat: ensure AccountMongoRepository returns an deposit on deposit success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,8 +3,11 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account/add-acc
 import { AccountModel } from '../../../../domain/models/account-model'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { LoadAccountByCpfRepository } from '../../../../data/protocols/load-account-by-cpf-repository'
+import { AlterMoneyAccountRepository } from '../../../../data/protocols/alter-money-account-repository'
+import { DepositAmountModel } from '../../../../domain/usecases/deposit-amount/deposit-amount'
+import { DepositModel } from '../../../../domain/models/deposit-model'
 
-export class AccountMongoRepository implements AddAccountRepository, LoadAccountByCpfRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByCpfRepository, AlterMoneyAccountRepository {
 
   async add (accountData: AddAccountModel): Promise<AccountModel>{
     const accountCollection = MongoHelper.getCollection('accounts')
@@ -17,6 +20,13 @@ export class AccountMongoRepository implements AddAccountRepository, LoadAccount
     const accountCollection = MongoHelper.getCollection('accounts')
     const account = await accountCollection.findOne({cpf})
     return new Promise(resolve => resolve(account && MongoHelper.map(account)))
+  }
+
+  async deposit (depositData: DepositAmountModel): Promise<DepositModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({cpf: depositData.cpf}, {$inc: {money: depositData.depositValue}})
+    const account = await accountCollection.findOne({cpf: depositData.cpf})
+    return {name: account.name, cpf: account.cpf, depositValue: depositData.depositValue}
   }
 
 }
