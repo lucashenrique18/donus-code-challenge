af781df42ba219a20f44ab2cc04622a38f66e418
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-20 23:20:01 -0300

    test: ensure DepositAccountUseCase callss AlterMoneyrAccountRepository deposit with correct values

---
+++ src/data/usecases/deposit-account/deposit-account.spec.ts
@@ -0,0 +1,58 @@
+import { DepositAccount } from './deposit-account'
+import { DepositModel } from "../../../domain/models/deposit-model"
+import { AlterMoneyAccountRepository } from "../../protocols/alter-money-account-repository"
+
+const validDeposit = 100
+const validAccount = {
+  name: 'any_name',
+  cpf: 'any_cpf',
+  depositValue: validDeposit
+}
+const validDepositData = {
+  cpf: 'any_cpf',
+  password: 'any_password',
+  depositValue: validDeposit
+}
+
+const makeAlterMoneyAccountRepository = (): AlterMoneyAccountRepository => {
+  class AlterMoneyAccountRepositoryStub implements AlterMoneyAccountRepository {
+    async deposit (depositValue: number): Promise<DepositModel> {
+      return new Promise(resolve => resolve(validAccount))
+    }
+  }
+  return new AlterMoneyAccountRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DepositAccount
+  alterMoneyAccountRepositoryStub: AlterMoneyAccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const alterMoneyAccountRepositoryStub = makeAlterMoneyAccountRepository()
+  const sut = new DepositAccount(alterMoneyAccountRepositoryStub)
+  return {
+    sut,
+    alterMoneyAccountRepositoryStub
+  }
+}
+
+
+describe('Deposit Account UseCase', () => {
+
+  test('Should call AlterMoneyAccountRepository deposit with correct values', async () => {
+    const {sut, alterMoneyAccountRepositoryStub} = makeSut()
+    const loadSpy = jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit')
+    await sut.deposit(validDepositData)
+    expect(loadSpy).toHaveBeenCalledWith(validDeposit)
+  })
+
+  test('Should throw if AlterMoneyAccountRepository throws', async () => {
+    const { sut, alterMoneyAccountRepositoryStub } = makeSut()
+    jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.deposit(validDepositData)
+    await expect(promise).rejects.toThrow()
+  })
+
+})
+
