4d8bd3a8224c465966f04032111ba3bb5e77f1d5
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-21 12:25:34 -0300

    feat: ensure AccountMongoRepository returns an account on loadByCpf success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,14 +2,21 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AddAccountModel } from '../../../../domain/usecases/add-account/add-account'
 import { AccountModel } from '../../../../domain/models/account-model'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadAccountByCpfRepository } from '../../../../data/protocols/load-account-by-cpf-repository'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByCpfRepository {
 
   async add (accountData: AddAccountModel): Promise<AccountModel>{
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = MongoHelper.map(result.ops[0])
     return new Promise(resolve => resolve(account))
+  }
+
+  async loadByCpf (cpf: string): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({cpf})
+    return new Promise(resolve => resolve(MongoHelper.map(account)))
   }
 
 }
