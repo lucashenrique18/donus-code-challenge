b57336dcfa055bc0bab46d93f0b994e4a5abd2ea
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-22 02:45:15 -0300

    feat: ensure DbAddAccount calls LoadAccountByCpfRepository with correct cpf

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,9 +1,10 @@
-import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter, AddAccountRepository, LoadAccountByCpfRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter, private readonly addAccountRepository: AddAccountRepository) {}
+  constructor (private readonly encrypter: Encrypter, private readonly addAccountRepository: AddAccountRepository, private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByCpfRepository.loadByCpf(accountData.cpf)
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { cpf: accountData.cpf.replace(/([-.]*)/g, ''), password: hashedPassword, money: 0 }))
     return new Promise(resolve => resolve(account))
