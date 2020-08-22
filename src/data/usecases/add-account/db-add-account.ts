2641ad32c22ccbcec8b8d75d496e5f1192c4fa46
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-22 02:53:06 -0300

    feat: ensure DbAddAccount returns null if LoadAccountByCpfRepository returns not null

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -4,9 +4,12 @@ export class DbAddAccount implements AddAccount {
   constructor (private readonly encrypter: Encrypter, private readonly addAccountRepository: AddAccountRepository, private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    await this.loadAccountByCpfRepository.loadByCpf(accountData.cpf)
-    const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { cpf: accountData.cpf.replace(/([-.]*)/g, ''), password: hashedPassword, money: 0 }))
-    return new Promise(resolve => resolve(account))
+    const account = await this.loadAccountByCpfRepository.loadByCpf(accountData.cpf)
+    if (!account) {
+      const hashedPassword = await this.encrypter.encrypt(accountData.password)
+      const newAccount = await this.addAccountRepository.add(Object.assign({}, accountData, { cpf: accountData.cpf.replace(/([-.]*)/g, ''), password: hashedPassword, money: 0 }))
+      return new Promise(resolve => resolve(newAccount))
+    }
+    return null
   }
 }
