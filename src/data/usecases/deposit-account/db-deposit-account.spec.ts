04c5b9314312f56ca45eb27ed8af1ca369103743
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-23 20:47:52 -0300

    test: ensure DepositAccount calls AccountMovimentationHistoryRepository save with correct values

--- src/data/usecases/deposit-account/db-deposit-account.spec.ts
+++ src/data/usecases/deposit-account/db-deposit-account.spec.ts
@@ -1,7 +1,9 @@
 import { DepositAccount } from './db-deposit-account'
 import { DepositModel } from "../../../domain/models/deposit-model"
 import { AlterMoneyAccountRepository } from "../../protocols/db/account/alter-money-account-repository";
+import { AccountMovimentationHistoryRepository } from "../../protocols/db/account/account-movimentation-history-repository";
 import { DepositAmountModel } from '../../../domain/usecases/deposit-amount/deposit-amount'
+import { MovimentationModel } from '../../../domain/models/movimentation-model';
 
 const validDeposit = 100
 const depositWithBonus = validDeposit+(validDeposit*0.05)
@@ -25,17 +27,29 @@ const makeAlterMoneyAccountRepository = (): AlterMoneyAccountRepository => {
   return new AlterMoneyAccountRepositoryStub()
 }
 
+const makeAccountMovimentationHistoryRepository = (): AccountMovimentationHistoryRepository => {
+  class AccountMovimentationHistoryRepositoryStub implements AccountMovimentationHistoryRepository {
+    save (movimentation: MovimentationModel): void {
+      null
+    }
+  }
+  return new AccountMovimentationHistoryRepositoryStub()
+}
+
 interface SutTypes {
   sut: DepositAccount
   alterMoneyAccountRepositoryStub: AlterMoneyAccountRepository
+  accountMovimentationHistoryRepositoryStub: AccountMovimentationHistoryRepository
 }
 
 const makeSut = (): SutTypes => {
   const alterMoneyAccountRepositoryStub = makeAlterMoneyAccountRepository()
-  const sut = new DepositAccount(alterMoneyAccountRepositoryStub)
+  const accountMovimentationHistoryRepositoryStub = makeAccountMovimentationHistoryRepository()
+  const sut = new DepositAccount(alterMoneyAccountRepositoryStub, accountMovimentationHistoryRepositoryStub)
   return {
     sut,
-    alterMoneyAccountRepositoryStub
+    alterMoneyAccountRepositoryStub,
+    accountMovimentationHistoryRepositoryStub
   }
 }
 
@@ -44,9 +58,9 @@ describe('Deposit Account UseCase', () => {
 
   test('Should call AlterMoneyAccountRepository deposit with correct values', async () => {
     const {sut, alterMoneyAccountRepositoryStub} = makeSut()
-    const loadSpy = jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit')
+    const depositSpy = jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit')
     await sut.deposit(validDepositData)
-    expect(loadSpy).toHaveBeenCalledWith({
+    expect(depositSpy).toHaveBeenCalledWith({
       cpf: 'any_cpf',
       password: 'any_password',
       depositValue: depositWithBonus
@@ -58,6 +72,24 @@ describe('Deposit Account UseCase', () => {
     jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.deposit(validDepositData)
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should throw if AlterMoneyAccountRepository throws', async () => {
+    const { sut, alterMoneyAccountRepositoryStub } = makeSut()
+    jest.spyOn(alterMoneyAccountRepositoryStub, 'deposit').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = sut.deposit(validDepositData)
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call AccountMovimentationHistoryRepository save with correct values', async () => {
+    const {sut, accountMovimentationHistoryRepositoryStub} = makeSut()
+    const saveSpy = jest.spyOn(accountMovimentationHistoryRepositoryStub, 'save')
+    await sut.deposit(validDepositData)
+    expect(saveSpy).toHaveBeenCalledWith({
+      cpf: validDepositData.cpf,
+      movimentationType: 'deposit',
+      value: validDepositData.depositValue
+    })
   })
 
   test('Should return an account on success', async () => {
