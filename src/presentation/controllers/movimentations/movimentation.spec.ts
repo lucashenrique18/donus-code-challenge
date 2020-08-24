975c303a853c6e27ac238ea84978e46d53e858cd
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-23 23:48:10 -0300

    test: ensure MovimentationController returns 400 if cpf or password is provided

---
+++ src/presentation/controllers/movimentations/movimentation.spec.ts
@@ -0,0 +1,40 @@
+import { MovimentationController } from './movimentation'
+import { MissingParamError } from '../../errors'
+
+interface SutTypes {
+  sut: MovimentationController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new MovimentationController()
+  return {
+    sut
+  }
+}
+
+describe('Movimentation Controller', () => {
+  test('Should return 400 if no cpf is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('cpf'))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+})
