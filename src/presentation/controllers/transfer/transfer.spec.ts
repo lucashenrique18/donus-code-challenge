80146bb4e6771560a8be746340d21a1023187bcf
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-25 02:48:36 -0300

    test: ensure TransferController returns 400 if an invalid cpf is provided

--- src/presentation/controllers/transfer/transfer.spec.ts
+++ src/presentation/controllers/transfer/transfer.spec.ts
@@ -1,16 +1,29 @@
-import { MissingParamError } from '../../errors'
+import { MissingParamError, InvalidParamError } from '../../errors'
 import { TransferController } from './transfer'
+import { CpfValidator } from '../../protocols/cpf-validator'
 
-const validDepositValue = 100
+const validValue = 100
+
+const makeCpfValidator = (): CpfValidator => {
+  class CpfValidatorStub implements CpfValidator {
+    isValid (cpf: string): boolean {
+      return true
+    }
+  }
+  return new CpfValidatorStub()
+}
 
 interface SutTypes {
   sut: TransferController
+  cpfValidatorStub: CpfValidator
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new TransferController()
+  const cpfValidatorStub = makeCpfValidator()
+  const sut = new TransferController(cpfValidatorStub)
   return {
-    sut
+    sut,
+    cpfValidatorStub
   }
 }
 
@@ -20,8 +33,8 @@ describe('Transfer Controller', () => {
     const httpRequest = {
       body: {
         password: 'any_password',
-        beneficiary: 'any_beneficiary',
-        value: validDepositValue
+        beneficiaryCpf: 'any_beneficiary',
+        value: validValue
       }
     }
     const httpResponse = await sut.handle(httpRequest)
@@ -34,8 +47,8 @@ describe('Transfer Controller', () => {
     const httpRequest = {
       body: {
         cpf: 'any_cpf',
-        beneficiary: 'any_beneficiary',
-        value: validDepositValue
+        beneficiaryCpf: 'any_beneficiary',
+        value: validValue
       }
     }
     const httpResponse = await sut.handle(httpRequest)
@@ -43,18 +56,18 @@ describe('Transfer Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  test('Should return 400 if no beneficiary is provided', async () => {
+  test('Should return 400 if no beneficiaryCpf is provided', async () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
         cpf: 'any_cpf',
         password: 'any_password',
-        value: validDepositValue
+        value: validValue
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingParamError('beneficiary'))
+    expect(httpResponse.body).toEqual(new MissingParamError('beneficiaryCpf'))
   })
 
   test('Should return 400 if no value is provided', async () => {
@@ -63,12 +76,28 @@ describe('Transfer Controller', () => {
       body: {
         cpf: 'any_cpf',
         password: 'any_password',
-        beneficiary: 'any_beneficiary'
+        beneficiaryCpf: 'any_beneficiary'
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('value'))
+  })
+
+  test('Should return 400 if an invalid cpf is provided ', async () => {
+    const { sut, cpfValidatorStub } = makeSut()
+    jest.spyOn(cpfValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        password: 'any_password',
+        beneficiaryCpf: 'any_beneficiary',
+        value: validValue
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('cpf'))
   })
 
 })
