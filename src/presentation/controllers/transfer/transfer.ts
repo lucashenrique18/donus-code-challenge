f5b57e325556d8a5287f73dfdc668b95b3518887
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-25 02:22:21 -0300

    feat: ensure TransferController returns 400 if an invalid cpf is provided

--- src/presentation/controllers/transfer/transfer.ts
+++ src/presentation/controllers/transfer/transfer.ts
@@ -1,15 +1,23 @@
-import { MissingParamError } from '../../errors'
+import { MissingParamError, InvalidParamError } from '../../errors'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
+import { CpfValidator } from '../../protocols/cpf-validator'
 
 export class TransferController implements Controller {
 
+  constructor (private readonly cpfValidator: CpfValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['cpf', 'password', 'beneficiary', 'value' ]
+    const requiredFields = ['cpf', 'password', 'beneficiaryCpf', 'value' ]
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const { cpf, password, beneficiaryCpf, value } = httpRequest.body
+    const isValidCpf = this.cpfValidator.isValid(cpf)
+    if (!isValidCpf) {
+      return badRequest(new InvalidParamError('cpf'))
     }
   }
 
