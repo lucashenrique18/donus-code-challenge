0cb3fef19e8a905311e60bdab67c19f0e79f3e95
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-25 02:00:50 -0300

    test: ensure TransferController returns 400 if params is not provided

--- src/presentation/controllers/transfer/transfer.spec.ts
+++ src/presentation/controllers/transfer/transfer.spec.ts
@@ -1,6 +1,8 @@
 import { MissingParamError } from '../../errors'
 import { TransferController } from './transfer'
 
+const validDepositValue = 100
+
 interface SutTypes {
   sut: TransferController
 }
@@ -19,12 +21,54 @@ describe('Transfer Controller', () => {
       body: {
         password: 'any_password',
         beneficiary: 'any_beneficiary',
-        value: 100
+        value: validDepositValue
       }
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('cpf'))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        beneficiary: 'any_beneficiary',
+        value: validDepositValue
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
+  test('Should return 400 if no beneficiary is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        password: 'any_password',
+        value: validDepositValue
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('beneficiary'))
+  })
+
+  test('Should return 400 if no value is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        cpf: 'any_cpf',
+        password: 'any_password',
+        beneficiary: 'any_beneficiary'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('value'))
   })
 
 })
