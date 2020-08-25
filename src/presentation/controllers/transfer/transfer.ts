4e3fb40c0ed2218b6da05c20c2ae3f15bb09d99d
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-25 03:24:54 -0300

    feat: ensure TransferController returns 500 if CpfValidator throws

--- src/presentation/controllers/transfer/transfer.ts
+++ src/presentation/controllers/transfer/transfer.ts
@@ -1,6 +1,6 @@
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { CpfValidator } from '../../protocols/cpf-validator'
 
 export class TransferController implements Controller {
@@ -8,23 +8,29 @@ export class TransferController implements Controller {
   constructor (private readonly cpfValidator: CpfValidator) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFields = ['cpf', 'password', 'beneficiaryCpf', 'value' ]
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['cpf', 'password', 'beneficiaryCpf', 'value' ]
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    const { cpf, password, beneficiaryCpf, value } = httpRequest.body
-    const isValidCpf = this.cpfValidator.isValid(cpf)
-    if (!isValidCpf) {
-      return badRequest(new InvalidParamError('cpf'))
-    }
-    const isValidBeneficiaryCpf = this.cpfValidator.isValid(beneficiaryCpf)
-    if (!isValidBeneficiaryCpf) {
-      return badRequest(new InvalidParamError('beneficiaryCpf'))
-    }
-    if (value <= 0) {
-      return badRequest(new InvalidParamError('value'))
+      const { cpf, password, beneficiaryCpf, value } = httpRequest.body
+      const isValidCpf = this.cpfValidator.isValid(cpf)
+      if (!isValidCpf) {
+        return badRequest(new InvalidParamError('cpf'))
+      }
+      const isValidBeneficiaryCpf = this.cpfValidator.isValid(beneficiaryCpf)
+      if (!isValidBeneficiaryCpf) {
+        return badRequest(new InvalidParamError('beneficiaryCpf'))
+      }
+      if (value <= 0) {
+        return badRequest(new InvalidParamError('value'))
+      }
+
+    } catch (error) {
+      console.error(error)
+      return serverError()
     }
   }
 
