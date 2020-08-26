70db0812561889e95ed9d46eb97b0fdeb7d01eef
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-26 05:14:46 -0300

    feat: ensure DbWithdrawMoney calls and throws correctly LoadAccountByCpfRepository

--- src/data/usecases/withdraw-money/db-withdraw-money.ts
+++ src/data/usecases/withdraw-money/db-withdraw-money.ts
@@ -2,11 +2,16 @@ import { WithdrawMoney, WithdrawModel } from "../../../domain/usecases/withdraw/
 import { AlterMoneyAccountRepository } from "../../protocols/db/account/alter-money-account-repository"
 import { AccountMovimentationHistoryRepository } from "../../protocols/db/account/account-movimentation-history-repository"
 import { WithdrawReturnModel } from "../../../domain/models/withdraw-return-model"
+import { LoadAccountByCpfRepository } from "../../protocols/db/account/load-account-by-cpf-repository"
 
 export class DbWithdrawMoney implements WithdrawMoney {
-  constructor (private readonly alterMoneyAccountRepository: AlterMoneyAccountRepository, private readonly accountMovimentationHistoryRepository: AccountMovimentationHistoryRepository) {}
+  constructor (private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository, private readonly alterMoneyAccountRepository: AlterMoneyAccountRepository, private readonly accountMovimentationHistoryRepository: AccountMovimentationHistoryRepository) {}
 
   async withdraw (withdrawData: WithdrawModel): Promise<WithdrawReturnModel> {
+    const account = await this.loadAccountByCpfRepository.loadByCpf(withdrawData.cpf)
+    if (account.money < withdrawData.value) {
+      return null
+    }
     const taxValue = withdrawData.value*0.01
     const withdraw = await this.alterMoneyAccountRepository.withdraw(withdrawData)
     await this.accountMovimentationHistoryRepository.saveMovimentation({
