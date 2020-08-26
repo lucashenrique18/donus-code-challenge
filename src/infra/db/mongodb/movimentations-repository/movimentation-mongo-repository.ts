895a0a24581c5cd9b3ad0b80b93576beea50c794
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-25 23:41:22 -0300

    refactor: rename vars

--- src/infra/db/mongodb/movimentations-repository/movimentation-mongo-repository.ts
+++ src/infra/db/mongodb/movimentations-repository/movimentation-mongo-repository.ts
@@ -4,8 +4,8 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class MovimentationMongoRepository implements LoadMovimentationRepository {
 
   async loadMovimentations (cpf: string): Promise<Array<LoadMovimentationModel>> {
-    const accountCollection = await MongoHelper.getCollection('movimentations')
-    const movimentations = await accountCollection.find({cpf}).toArray()
+    const movimentationsCollection = await MongoHelper.getCollection('movimentations')
+    const movimentations = await movimentationsCollection.find({cpf}).toArray()
     return new Promise(resolve => resolve(movimentations && movimentations.map(({_id, ...rest}) => Object.assign({}, rest, { id: _id }))))
   }
 }
