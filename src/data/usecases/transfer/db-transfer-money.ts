f102e97bb04b418ad4d179c1e0abd51c66170939
Lucas Henrique <lucashenrique_18@outlook.com>
2020-08-26 01:21:02 -0300

    feat: ensure DbTransferMoney calls AlterMoneyAccountRepository with correct values

--- src/data/usecases/transfer/db-transfer-money.ts
+++ src/data/usecases/transfer/db-transfer-money.ts
@@ -1,10 +1,11 @@
 import { TransferMoney, TransferModel } from "../../../domain/usecases/transfer-money/transfer-money"
 import { TransferMoneyModel } from "../../../domain/models/transfer-money-model"
 import { LoadAccountByCpfRepository } from "../../protocols/db/account/load-account-by-cpf-repository"
+import { AlterMoneyAccountRepository } from "../../protocols/db/account/alter-money-account-repository"
 
 export class DbTransferMoney implements TransferMoney {
 
-  constructor (private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository) {}
+  constructor (private readonly loadAccountByCpfRepository: LoadAccountByCpfRepository, private readonly alterMoneyAccountRepository: AlterMoneyAccountRepository) {}
 
   async transfer (transferMoney: TransferModel): Promise<TransferMoneyModel> {
     const account = await this.loadAccountByCpfRepository.loadByCpf(transferMoney.cpf)
@@ -15,6 +16,7 @@ export class DbTransferMoney implements TransferMoney {
     if (!beneficiary) {
       return undefined
     }
+    await this.alterMoneyAccountRepository.transfer(transferMoney)
     return null
   }
 }
